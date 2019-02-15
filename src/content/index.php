dec68f503f8523eb80de893e95d7a911b5bc5297
tricialeach <tleach@gmail.com>
2019-02-15 00:23:35 -0800

    add note to source, remove jquery

--- src/content/index.php
+++ src/content/index.php
@@ -1,3 +1,8 @@
+<!-- 👋 Hello there visitor!
+        I'm in the process of updating the code on this site as of Feb 2019.
+        I've learned a lot since I started working professionally.
+        You can peep on my progress here: https://github.com/tricialeach/tricialea.ch
+-->
 <!DOCTYPE html>
 <html lang="en-us">
   <?php include_once 'head.php' ?>
@@ -13,7 +18,6 @@
       </div>
     </div>
     <?php include_once 'footer.php' ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="/assets/js/scripts.js"></script>
   </body>
 </html>
